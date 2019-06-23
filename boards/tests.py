e50cf61f2d8b7babaf85a37a0b1c6d04eaddb105
a1685823 <a1685823@student.adelaide.edu.au>
2019-06-23 23:10:20 +0930

    Added separate topic pages for each board.

--- boards/tests.py
+++ boards/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 # Create your tests here.
 class HomeTests(TestCase):
@@ -12,3 +13,24 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+class BoardTopics(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    # test if board is found
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # test if board not found
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    # test if correct view is used to render templates
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
